834a4c8cd97dcbcbee30e0144bfea91e10571e12
MdRehanAli <rehanali21d@gmail.com>
2024-05-08 11:21:11 +0600

    Added mongo

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const express = require('express');
 
 const app = express();
 
-
 const port = process.env.PORT || 8000
+
+// Mongo Connnection 
+
+// const dbURI = ""
+// mongoose.connect(dbURi)
+//     .then((result) => {app.listen(4000)})
+//     .catch(err => console.log(err))
+
 
 // ejs setup
 app.set('view engine', 'ejs')
@@ -12,6 +19,11 @@ app.set('views', './views')
 
 // route setup 
 app.get('/', (req, res) => {
+    const blogs = [
+        { title: "Rehan", snepet: "Lorem ipsum 1" },
+        { title: "Rehan 2", snepet: "Lorem ipsum 2" },
+        { title: "Rehan 3", snepet: "Lorem ipsum 3" }
+    ]
     res.render('index')
 })
 app.get('/about', (req, res) => {
